0dfa52b6bd4e4ba1973e3895cbfde2f123f780e7
Justin Byrne <theskaterjesus@gmail.com>
2022-10-28 11:55:16 +0100

    Added css classess for tailwind

--- config/a-bunch-of-livewire-components.php
+++ config/a-bunch-of-livewire-components.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-    //
+    /**
+     * Autocomplete TailwindCSS classes.
+     */
+    'autocomplete' => [
+        'classes' => [
+            'input' => 'block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
+            'panel' => 'absolute left-0 w-full mt-2 overflow-y-auto bg-white rounded-md shadow-md max-h-52',
+            'item' => 'p-2 hover:bg-cyan-100',
+        ],
+    ],
 ];
